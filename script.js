f92597ba5a0eeee4ce5bb9b0fa28ecd603d9fc00
vhoughton <vhoughtonp@gmail.com>
2024-08-17 22:54:01 +0200

    menu

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("hamburger").addEventListener("click", function() {
+    const navLinks = document.getElementById("nav-links");
+    navLinks.classList.toggle("active");
+});
