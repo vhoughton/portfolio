8349bdba04a8b3c8b50545295d42aba16de483e7
vhoughton <vhoughtonp@gmail.com>
2024-08-19 19:33:05 +0200

    anchor

--- script.js
+++ script.js
@@ -2,3 +2,18 @@ document.getElementById("hamburger").addEventListener("click", function() {
     const navLinks = document.getElementById("nav-links");
     navLinks.classList.toggle("active");
 });
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+      e.preventDefault();
+  
+      const target = document.querySelector(this.getAttribute('href'));
+      const offset = 24; // 24px offset
+  
+      window.scrollTo({
+        top: target.offsetTop - offset,
+        behavior: 'smooth'
+      });
+    });
+  });
+  
